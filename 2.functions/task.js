6e4bfa207efbd0a0567e6e5749564e4cee5ada0c
Mikhail Polovnikov <mishapolovnikov@gmail.com>
2023-03-27 18:14:10 +0500

    change2 task.js

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,24 +1,81 @@
-function getArrayParams(...arr) {
+"use strick";
 
-  return { min: min, max: max, avg: avg };
+function getArrayParams(...arr) {
+  let min = Math.min(...arr);
+  let max = Math.max(...arr);
+  let sum = 0;
+    
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  let avg = parseFloat((sum / arr.length).toFixed(2));
+  return {min: min, max: max, avg: avg};
 }
 
+
 function summElementsWorker(...arr) {
+  if (arr.length === 0) {
+    return 0;
+  }
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum;
 
 }
 
 function differenceMaxMinWorker(...arr) {
-
+  if (arr.length === 0) {
+    return 0;
+  }
+  let max = Math.max(...arr);
+  let min = Math.min(...arr);
+  return (max - min);
 }
 
 function differenceEvenOddWorker(...arr) {
-
+  if (arr.length === 0) {
+    return 0;
+  }
+  let sumEvenElement = 0;
+  let sumOddElement = 0;
+  for (let i = 0; i < arr.length; i++){
+    if (arr[i] % 2 === 0) {
+      sumEvenElement += arr[i];
+    } 
+    else {
+      sumOddElement += arr[i];
+    }
+  }
+  return (sumEvenElement - sumOddElement);
 }
 
 function averageEvenElementsWorker(...arr) {
+  if (arr.length === 0) {
+    return 0;
+  }
+
+  let sumEvent = 0;
+  let countEvent = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+      sumEvent += arr[i];
+      countEvent += 1;
+    }
+  }
+  return (sumEvent / countEvent);
 
 }
 
 function makeWork (arrOfArr, func) {
-
+  let maxWorkerResult = func(...arrOfArr[0]);
+  for (let i = 0; i < arrOfArr.length; i++) {
+    const result = func(...arrOfArr[i]);
+    if (result > maxWorkerResult) {
+      maxWorkerResult = result;
+    }
+  }
+  return maxWorkerResult;
 }
