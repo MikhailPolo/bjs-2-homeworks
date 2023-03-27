ecdf5cbeb998a4e6c262025ca9654adce6e5f5f5
Mikhail Polovnikov <mishapolovnikov@gmail.com>
2023-03-27 18:16:31 +0500

    change3 task.js

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -38,7 +38,7 @@ function differenceEvenOddWorker(...arr) {
   if (arr.length === 0) {
     return 0;
   }
-  let sumEvenElement = 0;
+  let sumEvenElement = 0;  
   let sumOddElement = 0;
   for (let i = 0; i < arr.length; i++){
     if (arr[i] % 2 === 0) {
