623457490a656edc51267b6f6e0ba17c1a730d79
Mikhail Polovnikov <mishapolovnikov@gmail.com>
2023-04-01 16:54:21 +0500

    change 3.arrays

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,24 @@
+"use strict"
+
 function compareArrays(arr1, arr2) {
-  
+    if (arr1.length === arr2.length) {
+        return arr1.every((element, i) => element === arr2[i]);
+    }
+    return false;
 }
 
-function getUsersNamesInAgeRange(users, gender) {
-  
+function getUsersNamesInAgeRange(people, gender) {
+    if ((gender !== "мужской" && gender !== "женский") || (people === [])) {
+        return 0;
+    } else {
+        return people.filter(people => people.gender === gender)
+        .map(people => people.age)
+        .reduce((peopleAge, item, index, arr) => {
+            peopleAge += item;
+            if (index === arr.length - 1) {
+                return peopleAge / arr.length
+            }
+            return peopleAge;
+        }, 0)
+    } 
 }
