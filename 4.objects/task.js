e79f3a28a3bf83eff0719f92c1d8b5adb052cb5b
Mikhail Polovnikov <mishapolovnikov@gmail.com>
2023-04-01 22:28:59 +0500

    change 4.object

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,34 @@
+"use strict"
+
 function Student(name, gender, age) {
-  
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
+    return this.subject = subjectName;
   
 }
 
-Student.prototype.addMarks = function (...marks) {
-  
+Student.prototype.addMarks = function (...marksAdd) {
+    if ("marks" in this) {
+        this.marks.push(...marksAdd);
+    }
 }
 
 Student.prototype.getAverage = function () {
-  
+    if ("marks" in this && this.marks.length !==0) {
+        return this.marks.reduce((marks, addMarks) => marks + addMarks, 0) / this.marks.length;
+    }
+    else {
+        return 0;
+    }    
 }
 
 Student.prototype.exclude = function (reason) {
-  
+    delete this.subject;
+    delete this.marks;
+    return this.excluded = reason;
 }
